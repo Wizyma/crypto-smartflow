0353fa5e4af119f4c06357a79bff1d3abb051d26
André Gomes <andre.gomes@live.fr>
2020-07-13 16:46:53 +0200

    style: 💄 add pointer cursor to navigation

--- src/components/CardHeader/index.tsx
+++ src/components/CardHeader/index.tsx
@@ -17,10 +17,20 @@ export default function CardHeader({
           display: 'flex',
         }}
       >
-        <Text style={{ margin: '1rem 5px' }} b={tab === 'list'} id="list" onClick={onTabChange}>
+        <Text
+          style={{ margin: '1rem 5px', cursor: 'pointer' }}
+          b={tab === 'list'}
+          id="list"
+          onClick={onTabChange}
+        >
           List
         </Text>
-        <Text style={{ margin: '1rem 5px' }} b={tab === 'graph'} id="graph" onClick={onTabChange}>
+        <Text
+          style={{ margin: '1rem 5px', cursor: 'pointer' }}
+          b={tab === 'graph'}
+          id="graph"
+          onClick={onTabChange}
+        >
           Graph
         </Text>
       </div>
